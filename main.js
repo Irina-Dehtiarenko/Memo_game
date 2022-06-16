f24a6b5492f82bea7c539b8373c95150f8901675
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-06-16 14:40:44 +0200

    final game

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 const colors = ['red', 'red', 'blue', 'blue', 'yellow', 'yellow', 'green', 'green', 'brown', 'brown', 'gray', 'gray', 'violet', 'violet', 'lightgreen', 'lightgreen', 'cadetblue', 'cadetblue']
 
-let divs = [...document.querySelectorAll('div')]//żeby ta zmienna była prawdziwą tablicą
-
+let divs = [...document.querySelectorAll('div')]
 let activeCard = ''
 let activeCards = []
 
 const gamePairs = colors.length / 2
 let gameResult = 0
-const startTime = new Date().getTime();// to jest objekt, sama Date - to pokazuje aktualny czas i cała date, ale metoda - getTime() - konwertuje to do milisekund
-console.log(startTime)
+const startTime = new Date().getTime();
 
 const showColor = (e) => {
 	e.target.classList.remove('hidden')
@@ -29,33 +27,25 @@ const showColor = (e) => {
 	setTimeout(() => {
 
 		if (activeCards[0].className === activeCards[1].className) {
-			console.log('Win')
 			activeCards.forEach(element => element.classList.add('off'))
 
-
-			//FUNKCJA W METODZIE FILTER()M= MUSI COŚ ZWRACAĆ, CZYLI 'RETURN'(ale jeżeli napisać jednoliniowo i bez klamrek, to automatycznie to zwróci nasz kod):
-			// divs = divs.filter(div => !div.classList.contains("off"))
 			divs = divs.filter(div => {
 				return !div.classList.contains("off")
 			})
 
 			gameResult++
 
-			console.log(gameResult)
 			if (gameResult === gamePairs) {
-				console.log('Wygrana')
 				const endTime = new Date().getTime()
 
 				const gameTime = Math.round((endTime - startTime) / 1000)
 
 				alert(`Wygraleś! Twój czas to ${gameTime} sekund(y)`)
 
-				location.reload()//odświeża stronę - graj od początku
+				location.reload()
 			}
 
 		} else {
-			console.log('Lose')
-
 			activeCards.forEach(element => element.classList.add('hidden'))
 		}
 		activeCard = ''
